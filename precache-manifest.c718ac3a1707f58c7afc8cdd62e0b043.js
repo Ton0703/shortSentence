8cb3ca5660cef805c80dd8f06e3501ea3882875e
Ton0703 <1120367213@qq.com>
2020-06-29 14:03:02 +0800

    Updates

---
+++ precache-manifest.c718ac3a1707f58c7afc8cdd62e0b043.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6d88e78331e3ba3c41dd35227efbbbb",
+    "url": "/shortSentence/index.html"
+  },
+  {
+    "revision": "37ddc5f0d6d0bee509e7",
+    "url": "/shortSentence/static/css/main.fdf7c673.chunk.css"
+  },
+  {
+    "revision": "fc2c50efd655430b9b7c",
+    "url": "/shortSentence/static/js/2.ee2ceb93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shortSentence/static/js/2.ee2ceb93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37ddc5f0d6d0bee509e7",
+    "url": "/shortSentence/static/js/main.78588e36.chunk.js"
+  },
+  {
+    "revision": "cc6f9fe75cec912b625e",
+    "url": "/shortSentence/static/js/runtime-main.cc69ce62.js"
+  }
+]);
